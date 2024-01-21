736f90d4952cca6a98b01c4d1277c89d0bd9b668
Tatsiana Sh <tatsiana15az@gmail.com>
2024-01-21 10:28:38 -0700

    Update main layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout";
 import FeedPage from "./pages/FeedPage";
 import UserPage from "./pages/UserPage";
 import DetailsLayout from "./layouts/DetailsLayout";
+import GroupsPage from "./pages/GroupsPage";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<MainLayout />}>
 					<Route index element={<FeedPage />} />
+					<Route path="/groups" element={<GroupsPage />} />
 				</Route>
 
 				<Route element={<DetailsLayout />}>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,28 +1,60 @@
-import { Typography } from "@mui/material";
+import {
+	Button,
+	Container,
+	Grid,
+	List,
+	ListItemButton,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import useSession from "../hooks/useSession";
 import MainContainer from "../containers/MainContainer";
+import theme from "../theme/theme";
 
 const MainLayout = () => {
 	const { session } = useSession();
 
 	return (
 		<MainContainer>
-			<Typography variant="h1" color="primary">
-				The Plant Stand
-			</Typography>
-			<Typography
-				variant="subtitle1"
-				color="secondary"
-				component={Link}
-				to={`/user/${session?.user?.id}`}
-			>
-				{session?.user?.displayName}
-			</Typography>
+			<Container maxWidth="lg">
+				<Grid container width={"100%"}>
+					<Grid item xs={12} md={2} spacing={2} gap={2}>
+						<Typography variant="h4" color="primary">
+							{session?.user?.displayName}
+						</Typography>
 
-			<div>MainLayout</div>
-			<Outlet />
+						<Link to={`/user/${session?.user?.id}`}>
+							<Button variant="contained"> View My Profile</Button>
+						</Link>
+						<nav>
+							<List>
+								<ListItemButton component={Link} to="/">
+									<ListItemText primary="My Feed" />
+								</ListItemButton>
+								<ListItemButton component={Link} to="/groups">
+									<ListItemText primary="My Groups" />
+								</ListItemButton>
+							</List>
+						</nav>
+					</Grid>
+					<Grid item xs={12} md={7}>
+						<Outlet />
+					</Grid>
+					<Grid item xs={12} md={3} pl={4}>
+						<Typography variant="h5" color="secondary">
+							Friends suggestions
+						</Typography>
+						<Link
+							to="/user/3"
+							style={{ textDecoration: "none", color: theme.palette.primary.main }}
+						>
+							Rising Star
+						</Link>
+					</Grid>
+				</Grid>
+			</Container>
 		</MainContainer>
 	);
 };
